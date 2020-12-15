367ea1725faf901ee124f94d5aea7b5bc952c237
tam nguyen <ndtam.273@gmail.com>
2020-12-15 18:09:54 +0700

    c9

--- Sources/App/Practice/ScoreView.swift
+++ Sources/App/Practice/ScoreView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2020 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,39 +32,28 @@
 
 import SwiftUI
 
-struct ChallengeView: View {
+struct ScoreView: View {
     // MARK: - Properties
-    let challengeTest: ChallengeTest
-    @State var showAnswers = false
+    var _numberOfAnswered = State<Int>(initialValue: 0)
+    var numberOfQuestions = 5
     var body: some View {
-        VStack {
-            Button(action: {
-                self.showAnswers = !self.showAnswers
-            }) {
-                QuestionView(question: challengeTest.challenge.question)
-                    .frame(height: 300)
-            }
-            ScoreView()
-            if showAnswers {
-                Divider()
-                ChoicesView(challengeTest: challengeTest)
-                    .frame(height: 300)
-                    .padding()
+        Button(action: {
+            self._numberOfAnswered.wrappedValue += 1
+            print("Answered: \(self._numberOfAnswered.wrappedValue)")
+        }) {
+            HStack {
+                Text("\(_numberOfAnswered.wrappedValue)/\(numberOfQuestions)")
+                    .font(.caption)
+                    .padding(4)
+                Spacer()
             }
         }
+        
     }
 }
 
-struct ChallengeView_Previews: PreviewProvider {
-    static let challengeTest = ChallengeTest(
-        challenge: Challenge(
-            question: "おねがいします",
-            pronunciation: "onegaishimasu",
-            answer: "Please"),
-        answers: ["Thank you", "Hello", "Goodbye"]
-    )
-    
+struct ScoreView_Previews: PreviewProvider {
     static var previews: some View {
-         ChallengeView(challengeTest: challengeTest)
+        ScoreView()
     }
 }
